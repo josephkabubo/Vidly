9571c61c2b60ed63c7ff8f68e5c85b5c9b09b008
Joseph Kabubo <joe.kabubo@gmail.com>
2020-09-01 14:03:07 +0300

    Solution - Section 2

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -12,6 +12,21 @@ namespace Vidly.Controllers
     {
         [Route("movies/release/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1,12)}")]
 
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
         // GET: Movies/Random
         public ActionResult Random()
         {
